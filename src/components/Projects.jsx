8c5f193aa33ac27eba605fd4afd811acf50f8817
Akshit Agrawal <71216807+justAkshitAgrawal@users.noreply.github.com>
2022-10-21 21:28:54 +0530

    Few Changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,13 +18,19 @@ function Projects() {
           img="https://i.ibb.co/pjCz09w/image.png"
           title="React Portfolio"
           desc="A portfolio website built using ReactJS and TailwindCSS."
-          link="https://github.com/justAkshitAgrawal/react-portfolio"
+          link="https://akshitagrawal.dev"
         />
         <Project
           img="https://i.ibb.co/Ykn9KtQ/image.png"
           title="Netflix Clone"
           desc="A Netflix clone built using ReactJS."
-          link="https://github.com/justAkshitAgrawal/netflix-clone"
+          link="https://netflix-clone-mu-lyart.vercel.app/"
+        />
+        <Project
+          img="https://i.ibb.co/nfwNGXy/image.png"
+          title="Todo App"
+          desc="A basic todo app built using ReactJS."
+          link="https://todoist-woad.vercel.app/"
         />
       </div>
     </motion.div>
